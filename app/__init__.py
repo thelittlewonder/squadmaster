3f36c1af1f2834c183e471d56636e811e56a7bea
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-05 14:40:48 +0530

    flasklogin added

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+login_manager = LoginManager()
 # db object which we will use to interact with the database.
 
 
@@ -20,9 +21,8 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
-
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    login_manager.init_app(app)
+    login_manager.login_message = "You are not authorised to see this page. Please log in"
+    login_manager.login_view = "auth.login"
 
     return app
