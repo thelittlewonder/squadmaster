814e551efe317b6b9d318325a1becc54bd0745c4
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-08 01:45:53 +0530

    department model added

--- app/models.py
+++ app/models.py
@@ -14,32 +14,39 @@ class Employee(UserMixin, db.Model):
     first_name = db.Column(db.String(60), index=True)
     last_name = db.Column(db.String(60), index=True)
     password_hash = db.Column(db.String(128))
-    department_id = db.Column(db.integer, db.ForeignKey(departments.id))
+    department_id = db.Column(db.integer, db.ForeignKey(departments.dep_id))
     role_id = db.Column(db.integer, db.ForeignKey(roles.id))
     is_admin = db.Column(db.Boolean, default=False)
 
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute.')
 
-@property
-def password(self):
-    raise AttributeError('password is not a readable attribute.')
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def verifypassword(self, password):
+        return check_password_hash(self.password_hash, password)
 
-@password.setter
-def password(self, password):
-    self.password_hash = generate_password_hash(password)
-
-
-def verifypassword(self, password):
-    return check_password_hash(self.password_hash, password)
-
-
-def __repr__(self):
-    return '<Employee: {}>'.format(self.username)
+    def __repr__(self):
+        return '<Employee: {}>'.format(self.username)
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return Employee.query.get(int(user_id))
 
-    
 
+class Department(db.Model):
+
+    __tablename__ = 'departments'
+
+    dep_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship(
+        'Employee', backref='department', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Department: {}>'.format(self.name)
