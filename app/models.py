4c80ce8ff3747b4a3c6bb088a12d415091cb498a
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-08 01:47:09 +0530

    role model added:oh that rhymes

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Employee(UserMixin, db.Model):
     last_name = db.Column(db.String(60), index=True)
     password_hash = db.Column(db.String(128))
     department_id = db.Column(db.integer, db.ForeignKey(departments.dep_id))
-    role_id = db.Column(db.integer, db.ForeignKey(roles.id))
+    role_id = db.Column(db.integer, db.ForeignKey(roles.role_id))
     is_admin = db.Column(db.Boolean, default=False)
 
     @property
@@ -50,3 +50,17 @@ class Department(db.Model):
 
     def __repr__(self):
         return '<Department: {}>'.format(self.name)
+
+
+class Role(db.Model):
+
+    __tablename__ = 'roles'
+
+    role_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship(
+        'Employee', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
