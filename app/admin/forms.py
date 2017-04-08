751400e774858aefccfc0d85b91a5d8cace7b379
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-09 02:23:19 +0530

    employee view updated

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from ..models import Department, Role
 
 
 class DepartmentForm(FlaskForm):
@@ -12,4 +14,12 @@ class DepartmentForm(FlaskForm):
 class RoleForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmployeeAssignForm(FlaskForm):
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
     submit = SubmitField('Submit')
--- app/admin/views.py
+++ app/admin/views.py
@@ -2,9 +2,9 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from forms import DepartmentForm, RoleForms
+from forms import DepartmentForm, RoleForm, EmployeeAssignForm
 from .. import db
-from ..models import Department, Role
+from ..models import Department, Role, Employee
 
 
 def check_admin():
@@ -184,3 +184,46 @@ def delete_role(id):
     return redirect(url_for('admin.list_roles'))
 
     return render_template(title="Delete Role")
+
+
+@admin.route('/employees')
+@login_required
+def list_employees():
+    """
+    List all employees
+    """
+    check_admin()
+
+    employees = Employee.query.all()
+    return render_template('admin/employees/employees.html',
+                           employees=employees, title='Employees')
+
+
+@admin.route('/employees/assign/<int:id>', methods=['GET', 'POST'])
+@login_required
+def assign_employee(id):
+    """
+    Assign a department and a role to an employee
+    """
+    check_admin()
+
+    employee = Employee.query.get_or_404(id)
+
+    # prevent admin from being assigned a department or role
+    if employee.is_admin:
+        abort(403)
+
+    form = EmployeeAssignForm(obj=employee)
+    if form.validate_on_submit():
+        employee.department = form.department.data
+        employee.role = form.role.data
+        db.session.add(employee)
+        db.session.commit()
+        flash('You have successfully assigned a department and role.')
+
+        # redirect to the roles page
+        return redirect(url_for('admin.list_employees'))
+
+    return render_template('admin/employees/employee.html',
+                           employee=employee, form=form,
+                           title='Assign Employee')
