44975c4f301d6209b5f7a619b0deb17505665ee4
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-09 00:18:27 +0530

    admin dashboard added

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required,current_user
 
 from . import home
 
@@ -14,3 +14,10 @@ def homepage():
 @login_required
 def dashboard():
     return render_template('home/dashboard.html', title='Dashboard')
+
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    if not current_user.is_admin:
+        abort(403)
+    return render_template('home/admin_dashboard.html', title='Admin Dashboard')
