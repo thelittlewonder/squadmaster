eef59d262c884e70d8369579625a5160c5f7498b
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-08 15:51:18 +0530

    bootstrap added

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 # local imports
 from config import app_config
 
@@ -21,6 +22,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    Bootstrap(app)
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You are not authorised to see this page. Please log in"
