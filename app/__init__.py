e90ef06f670eb600aa69670f401a5b81bd07f87f
Abhishek Sharma <hello.abhishek@outlook.com>
2017-04-09 02:42:47 +0530

    structure modified

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 # app/__init__.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+
 # local imports
 from config import app_config
 
@@ -39,5 +40,17 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
 
     return app
